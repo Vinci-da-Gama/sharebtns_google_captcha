a448970cb8cdf4f403d01c265ec9003d2c8a94ff
c++gccOc <kelin.liao@csbengage.com>
2017-05-19 15:20:57 +1000

    success sessionstorage

--- app/app.route.js
+++ app/app.route.js
@@ -11,6 +11,38 @@
                 templateUrl: './_partials/initHome.html',
                 controller: 'homeCtrl',
                 controllerAs: 'hc'
+            })
+            .state('home.login', {
+                url: '/login',
+                templateUrl: './_partials/home/login.html',
+                controller: 'homeLoiginCtrl',
+                controllerAs: 'hlc'
+            })
+            .state('home.register', {
+                url: '/register',
+                templateUrl: './_partials/home/register.html',
+                controller: 'homeRegisterCtrl',
+                controllerAs: 'hrc'
+            })
+            .state('sharebtns', {
+                url: '/sharebtns',
+                views: {
+                    '': {
+                        templateUrl: './_partials/sbs.html',
+                        controller: 'sharebtnsCtrl',
+                        controllerAs: 'sc'
+                    },
+                    'sbl@sharebtns': {
+                        templateUrl: './_partials/sharebtns/sbleft.html',
+                        controller: 'sharebtnsLeftCtrl',
+                        controllerAs: 'slc'
+                    },
+                    'sbr@sharebtns': {
+                        templateUrl: './_partials/sharebtns/sbright.html',
+                        controller: 'sharebtnsRightCtrl',
+                        controllerAs: 'src'
+                    }
+                }
             });
 
     }]);
--- app/ctrl/home-ctrl.js
+++ app/ctrl/home-ctrl.js
@@ -4,6 +4,34 @@
 
     ctrlM.controller('homeCtrl', ['$scope', '$log', function($scope, $log) {
         $log.log('Home_Ctrl...');
+        var vm = this;
+        vm.setSessionStorage = function() {
+            var obj = {
+                long: 100,
+                short: 1
+            };
+            sessionStorage.setItem('come', JSON.stringify(obj));
+        };
+    }]);
+
+    ctrlM.controller('homeLoiginCtrl', ['$scope', '$log', function($scope, $log) {
+        $log.log('homeLoiginCtrl...');
+    }]);
+
+    ctrlM.controller('homeRegisterCtrl', ['$scope', '$log', function($scope, $log) {
+        $log.log('homeRegisterCtrl...');
+    }]);
+
+    ctrlM.controller('sharebtnsCtrl', ['$scope', '$log', function($scope, $log) {
+        $log.log('sharebtnsCtrl...');
+    }]);
+
+    ctrlM.controller('sharebtnsLeftCtrl', ['$scope', '$log', function($scope, $log) {
+        $log.log('sharebtnsLeftCtrl...');
+    }]);
+
+    ctrlM.controller('sharebtnsRightCtrl', ['$scope', '$log', function($scope, $log) {
+        $log.log('sharebtnsRightCtrl...');
     }]);
 
 })();
